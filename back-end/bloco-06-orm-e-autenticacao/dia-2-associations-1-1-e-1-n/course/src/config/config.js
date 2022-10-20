601e1eb0408058ac20fb857056e165ab9348fcef
User <user@iMac-de-User.local>
2022-10-20 19:34:15 -0300

    course 6.2

---
+++ back-end/bloco-06-orm-e-autenticacao/dia-2-associations-1-1-e-1-n/course/src/config/config.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const config = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT,
+
+};
+
+module.exports = {
+  development: config,
+  test: config,
+  production: config,
+};
