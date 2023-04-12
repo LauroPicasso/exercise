b0798df24744f53b6641f41c1e2c93888d7137a6
LauroPicasso <picasso1231@gmail.com>
2023-04-12 14:27:54 -0300

    exemplo do course

---
+++ ciencias-da-computacao/bloco03/dia2/course/chrome.py
@@ -0,0 +1,17 @@
+# selenium 3
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys  # Importa teclas comuns
+
+chrome = webdriver.Chrome()
+
+response = chrome.get("https://www.google.com.br/")
+
+# pesquisa na instância aberta do navegador pela primeira ocorrência
+# do nome 'q'
+search_input = chrome.find_element("name", "q")
+
+# escreve selenium dentro do campo de pesquisa
+search_input.send_keys("selenium")
+
+# pressiona enter para realizar a busca
+search_input.send_keys(Keys.ENTER)
